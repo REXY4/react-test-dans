12040dbd02fd7dfd79759e32de2ac4d759a4783f
REXY4 <muhammadrzki96@gmail.com>
2023-03-06 19:03:02 +0700

    add husky eslint and else

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': 0,
+    'no-undef': 0,
+    'prettier/prettier': 0,
+  },
+};
